10ed9b1e77f5015c130290d9a7f1d44705530ae3
Tushar Sharma <tushar007king@gmail.com>
2025-01-14 20:48:59 +0530

    Ititial Commit

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const cors = require("cors");
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: "config/config.env" });
+}
+
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000","https://eureka-ecommerce-app.vercel.app"],  // Allow dev and prod origins
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
+    credentials: true, // Allow cookies and credentials
+  })
+);
+
+// Using Middlewares
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cookieParser());
+
+// Importing Routes
+const post = require("./routes/post");
+const user = require("./routes/user");
+
+// Using Routes
+app.use("/api/v1", post);
+app.use("/api/v1", user);
+
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
+
+module.exports = app;
